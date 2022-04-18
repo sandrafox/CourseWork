b0c62ff21b3a8786109837b7a5ab19a8a4789ac3
Alexandra Lisicyna <miss.lisicyna@gmail.com>
2022-04-18 21:26:14 +0300

    Get algo for hetero OneMax time

--- OneMax01/src/visualization/ExpectedOneMaxHe.java
+++ OneMax01/src/visualization/ExpectedOneMaxHe.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class ExpectedOneMaxHe extends ApplicationFrame {
@@ -42,7 +43,8 @@ public class ExpectedOneMaxHe extends ApplicationFrame {
         ChartPanel chartPanel = new ChartPanel( xylineChart );
         chartPanel.setPreferredSize( new java.awt.Dimension( 1000 , 800 ) );
         final XYPlot plot = xylineChart.getXYPlot( );
-        LogAxis yAxis = new LogAxis("Количество вычислений функции / длина^2");
+        //LogAxis yAxis = new LogAxis("Количество вычислений функции / длина^2");
+        LogAxis yAxis = new LogAxis("Количество вычислений функции (10^)");
         yAxis.setBase(10);
         LogFormat format = new LogFormat(yAxis.getBase(), "", "", true);
         yAxis.setNumberFormatOverride(format);
@@ -87,11 +89,15 @@ public class ExpectedOneMaxHe extends ApplicationFrame {
     }
 
     private BigDecimal calcP(int i, int n, BigDecimal mu) {
-        BigDecimal s = mu.multiply(BigDecimal.ONE.subtract(mu).pow(2 * n - 1)).multiply(new BigDecimal(2 * n - i)), cur  = BigDecimal.valueOf(s.doubleValue());
+        MathContext mc = new MathContext(10, RoundingMode.HALF_UP);
+        BigDecimal cur = (mu.multiply((BigDecimal.ONE.subtract(mu)).pow(2 * n - 1))).multiply((new BigDecimal(2 * n - i)).multiply(BigDecimal.TEN)), s  = BigDecimal.ZERO.add(cur);
         for (int t = 1; t <= (2* n - i - 1); t++) {
             int j = t - 1;
-            cur = cur.multiply(mu).multiply(mu).multiply(new BigDecimal(2 * n - i -j - 1)).multiply(new BigDecimal(i - n +j +1)).divide(BigDecimal.ONE.subtract(mu).pow(2).multiply(new BigDecimal(j + 2)).multiply(new BigDecimal(j + 1)), RoundingMode.HALF_UP);
+            cur = ((((cur.multiply(mu)).multiply(mu)).multiply(new BigDecimal(2 * n - i -j - 1))).multiply(new BigDecimal(i - n +j +1))).divide((((BigDecimal.ONE.subtract(mu)).pow(2)).multiply(new BigDecimal(j + 2))).multiply(new BigDecimal(j + 1)), mc);
             s = s.add(cur);
+        }
+        if (s.signum() < 0) {
+            System.out.println("i = " + i + " n = " + n + " mu = " + mu);
         }
         return s;
     }
@@ -102,59 +108,78 @@ public class ExpectedOneMaxHe extends ApplicationFrame {
         final XYSeries t05n = new XYSeries("2/N");
 
         myWriter.write("[{}\n");
-        for (int N = 5; N <= 12; ++N) {
+        MathContext mc = new MathContext(10, RoundingMode.HALF_UP);
+        for (int N = 5; N <= 11; ++N) {
             BigDecimal sn = BigDecimal.ZERO, s2n = BigDecimal.ZERO, s05n = BigDecimal.ZERO;
             final int n1 = (int) Math.ceil(Math.pow(2, N - 0.7)), n2 = (int) Math.ceil(Math.pow(2, N - 0.3)), n = 1 << N;
-            for (int i = n1 / 2; i < n1; i++) {
-                sn = sn.add(BigDecimal.ONE.divide(calcP(i, n1, BigDecimal.ONE.divide(BigDecimal.valueOf(n1), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s2n = s2n.add(BigDecimal.ONE.divide(calcP(i, n1, BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n1), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s05n = s05n.add(BigDecimal.ONE.divide(calcP(i, n1, BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n1), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
+            BigDecimal mu1 = BigDecimal.ONE.divide(BigDecimal.valueOf(n1), mc),
+                    mu2 = BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n1), mc),
+                    mu3 = BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n1), mc);
+            for (int i = n1; i < 2*n1; i++) {
+                sn = sn.add(BigDecimal.TEN.divide(calcP(i, n1, mu1), mc));
+                if (sn.signum() == -1) {
+                    System.out.println("1 i = " + i + " n = " + n1);
+                }
+                s2n = s2n.add(BigDecimal.TEN.divide(calcP(i, n1, mu2), mc));
+                if (sn.signum() == -1) {
+                    System.out.println("2 i = " + i + " n = " + n1);
+                }
+                s05n = s05n.add(BigDecimal.TEN.divide(calcP(i, n1, mu3), mc));
+                if (sn.signum() == -1) {
+                    System.out.println("3 i = " + i + " n = " + n1);
+                }
             }
-            sn = sn.divide(BigDecimal.valueOf(2L * n1 * n1), RoundingMode.HALF_UP);
-            s2n = s2n.divide(BigDecimal.valueOf(2L * n1 * n1), RoundingMode.HALF_UP);
-            s05n = s05n.divide(BigDecimal.valueOf(2L * n1 * n1), RoundingMode.HALF_UP);
+            sn = sn.divide(BigDecimal.valueOf(n1 * Math.log(n1)), mc);
+            s2n = s2n.divide(BigDecimal.valueOf(n1 * Math.log(n1)), mc);
+            s05n = s05n.divide(BigDecimal.valueOf(n1 * Math.log(n1)), mc);
             myWriter.write(",{\"fitness\":" + n1 + ",\"runtime\":" + sn + "}\n");
             myWriter.write(",{\"fitness\":" + n1 + ",\"runtime\":" + s2n + "}\n");
             myWriter.write(",{\"fitness\":" + n1 + ",\"runtime\":" + s05n + "}\n");
-            tn.add(n1, sn);
-            t2n.add(n1, s2n);
-            t05n.add(n1, s05n);
+            tn.add(2*n1, sn);
+            t2n.add(2*n1, s2n);
+            t05n.add(2*n1, s05n);
 
             sn = BigDecimal.ZERO;
             s2n = BigDecimal.ZERO;
             s05n = BigDecimal.ZERO;
-            for (int i = n / 2; i <= n; i++) {
-                sn = sn.add(BigDecimal.ONE.divide(calcP(i, n, BigDecimal.ONE.divide(BigDecimal.valueOf(n), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s2n = s2n.add(BigDecimal.ONE.divide(calcP(i, n, BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s05n = s05n.add(BigDecimal.ONE.divide(calcP(i, n, BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
+            mu1 = BigDecimal.ONE.divide(BigDecimal.valueOf(n), mc);
+            mu2 = BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n), mc);
+            mu3 = BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n), mc);
+            for (int i = n; i < 2*n; i++) {
+                sn = sn.add(BigDecimal.TEN.divide(calcP(i, n, mu1), mc));
+                s2n = s2n.add(BigDecimal.TEN.divide(calcP(i, n, mu2), mc));
+                s05n = s05n.add(BigDecimal.TEN.divide(calcP(i, n, mu3), mc));
             }
-            sn = sn.divide(BigDecimal.valueOf(2L * n * n), RoundingMode.HALF_UP);
-            s2n = s2n.divide(BigDecimal.valueOf(2L * n * n), RoundingMode.HALF_UP);
-            s05n = s05n.divide(BigDecimal.valueOf(2L * n * n), RoundingMode.HALF_UP);
+            sn = sn.divide(BigDecimal.valueOf(n * Math.log(n)), mc);
+            s2n = s2n.divide(BigDecimal.valueOf(n * Math.log(n)), mc);
+            s05n = s05n.divide(BigDecimal.valueOf(n * Math.log(n)), mc);
             myWriter.write(",{\"fitness\":" + n + ",\"runtime\":" + sn + "}\n");
             myWriter.write(",{\"fitness\":" + n + ",\"runtime\":" + s2n + "}\n");
             myWriter.write(",{\"fitness\":" + n + ",\"runtime\":" + s05n + "}\n");
-            tn.add(n, sn);
-            t2n.add(n, s2n);
-            t05n.add(n, s05n);
+            tn.add(2*n, sn);
+            t2n.add(2*n, s2n);
+            t05n.add(2*n, s05n);
 
             sn = BigDecimal.ZERO;
             s2n = BigDecimal.ZERO;
             s05n = BigDecimal.ZERO;
-            for (int i = n2 / 2; i <= n2; i++) {
-                sn = sn.add(BigDecimal.ONE.divide(calcP(i, n2, BigDecimal.ONE.divide(BigDecimal.valueOf(n2), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s2n = s2n.add(BigDecimal.ONE.divide(calcP(i, n2, BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n2), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
-                s05n = s05n.add(BigDecimal.ONE.divide(calcP(i, n2, BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n2), RoundingMode.HALF_UP)), RoundingMode.HALF_UP));
+            mu1 = BigDecimal.ONE.divide(BigDecimal.valueOf(n2), mc);
+            mu2 = BigDecimal.ONE.divide(BigDecimal.valueOf(2L * n2), mc);
+            mu3 = BigDecimal.valueOf(2).divide(BigDecimal.valueOf(n2), mc);
+            for (int i = n2; i < 2*n2; i++) {
+                sn = sn.add(BigDecimal.TEN.divide(calcP(i, n2, mu1), mc));
+                s2n = s2n.add(BigDecimal.TEN.divide(calcP(i, n2, mu2), mc));
+                s05n = s05n.add(BigDecimal.TEN.divide(calcP(i, n2, mu3), mc));
             }
-            sn = sn.divide(BigDecimal.valueOf(2L * n2 * n2), RoundingMode.HALF_UP);
-            s2n = s2n.divide(BigDecimal.valueOf(2L * n2 * n2), RoundingMode.HALF_UP);
-            s05n = s05n.divide(BigDecimal.valueOf(2L * n2 * n2), RoundingMode.HALF_UP);
+            sn = sn.divide(BigDecimal.valueOf(n2 * Math.log(n2)), mc);
+            s2n = s2n.divide(BigDecimal.valueOf(n2 * Math.log(n2)), mc);
+            s05n = s05n.divide(BigDecimal.valueOf(n2 * Math.log(n2)), mc);
             myWriter.write(",{\"fitness\":" + n2 + ",\"runtime\":" + sn + "}\n");
             myWriter.write(",{\"fitness\":" + n2 + ",\"runtime\":" + s2n + "}\n");
             myWriter.write(",{\"fitness\":" + n2 + ",\"runtime\":" + s05n + "}\n");
-            tn.add(n2, sn);
-            t2n.add(n2, s2n);
-            t05n.add(n2, s05n);
+            tn.add(2*n2, sn);
+            t2n.add(2*n2, s2n);
+            t05n.add(2*n2, s05n);
             System.out.println(N);
         }
 
